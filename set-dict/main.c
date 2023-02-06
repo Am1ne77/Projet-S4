4a62958849d0b1c2f5d9c8dc22a764714a877166
atti <attilio.levieils@epita.fr>
2023-02-06 22:52:07 +0100

    comments in set.c + better folder org

---
+++ set-dict/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+#include <string.h>
+#include "set.h"
+
+int main()
+{
+    
+
+    return 0;
+}
--- set-dict/set.c
+++ set-dict/set.c
@@ -25,70 +25,60 @@ uint32_t hash(char *key)
 // Return a new set of capacity capacity
 set* new_set(size_t capacity)
 {
-	set* set;
-   	set = malloc(sizeof(struct set));
-	if(set == NULL)
-		errx(1,"new_set() : Error !");
-	set->capacity = capacity;
-	set->size = 0;
-	set->elements = malloc(sizeof(struct data*) * capacity);
-	if(set->elements == NULL)
-		errx(1,"new_set() : Error !");
+    set* set;
+    set = malloc(sizeof(struct set));
+    if(set == NULL)
+        errx(1,"new_set() : Error !");
 
-        //allocate each cells of the array
-	for(size_t i = 0; i < capacity; ++i)
-	{
-		data* sentinel = malloc(sizeof(struct data));
-		if(sentinel == NULL)
-			errx(1,"new_set() : Error !");
-
-		sentinel->next = NULL;
-		set->elements[i] = sentinel;
-	}
-
-	return set;
-}
-
-/*
-
-set* new_set(size_t capacity) 
-{
-    set* set = (struct set*) malloc(sizeof(set));
     set->capacity = capacity;
     set->size = 0;
-    set->elements = (data**) malloc(sizeof(data*) * set->capacity);
-    memset(set->elements, 0, sizeof(data*) * set->capacity);
+
+    set->elements = malloc(sizeof(struct data*) * capacity);
+    if(set->elements == NULL)
+        errx(1,"new_set() : Error !");
+
+    //allocate each cells of the array
+    for(size_t i = 0; i < capacity; ++i)
+    {
+        data* sentinel = malloc(sizeof(struct data));
+        if(sentinel == NULL)
+        errx(1,"new_set() : Error !");
+
+        sentinel->next = NULL;
+        set->elements[i] = sentinel;
+    }
+
     return set;
-}*/
+}
 
 
 // free a set
 void free_set(set* set)
 {
-	for(size_t i = 0; i < set->capacity; ++i)
-	{
-		data* curr = set->elements[i];
-		while(curr != NULL)
-		{
-			data* next = curr->next;
-			free(curr);
-			curr = next;
-		}
-	}
-	free(set->elements);
-	free(set);
+    for(size_t i = 0; i < set->capacity; ++i)
+    {
+        data* curr = set->elements[i];
+        while(curr != NULL)
+        {
+            data* next = curr->next;
+            free(curr);
+            curr = next;
+        }
+    }
+    free(set->elements);
+    free(set);
 }
 
 
 // return 1 if key with hash i is in the set, 0 otherwise
 size_t search(set* set, char* key, uint32_t i)
 {
-
-    for(data* curr = (set->elements[i])->next; curr != NULL; curr = curr->next){
-	if(strcmp(key, curr->key) == 0)
-		return 1;
+    for(data* curr = (set->elements[i])->next; curr != NULL; curr = curr->next)
+    {
+        if(strcmp(key, curr->key) == 0)
+            return 1;
     }
-	return 0;
+    return 0;
 }
 
 
@@ -100,6 +90,7 @@ set* insert(set* set, char* key)
     if(search(set, key, i))
         return set;
 
+    // if threshold is reached double the capacity
     if(100 * set->size / set->capacity >= 75)
         expand_set(&set);
 
@@ -120,54 +111,68 @@ set* insert(set* set, char* key)
 // double the size of the set and recalculate new hash for all the elements
 void expand_set(set** p_set)
 {
+    // new set with double the capacity
     set* n_set = new_set(((*p_set)->capacity)*2);
-    
+
     for(size_t i = 0; i < (*p_set)->capacity; ++i)
     {
-        for(data* curr = (*p_set)->elements[i]->next; curr != NULL; curr = curr->next)
+        // reinsert the keys of the old set in the new set (new hash value)
+        data* curr = (*p_set)->elements[i]->next;
+        while(curr != NULL){
             n_set = insert(n_set, curr->key);
+            curr = curr->next;
+        }
     }
-    
+
     free_set(*p_set);
     *p_set = n_set;
 }
 
+// delete the key in the set, do nothing if key not in the set
 void delete(set* set, char* key)
 {
-	uint32_t i = hash(key) % (set->capacity);
-	data* curr = set->elements[i]->next;
-	data* past = set->elements[i];
-	while(curr != NULL)
-	{
-		if(strcmp(curr->key,key) == 0){
-			past->next = curr->next;
-			free(curr);
-			if(set->elements[i]->next == NULL)
-				set->size = set->size -1;
-                        break;
-		}
+    uint32_t i = hash(key) % (set->capacity);
 
-		past = curr;
-		curr = curr->next;
-	}
+    data* curr = set->elements[i]->next;
+    data* past = set->elements[i];
+    while(curr != NULL)
+    {
+        // if key found
+        if(strcmp(curr->key,key) == 0){
+            past->next = curr->next;
+            free(curr);
 
+            if(set->elements[i]->next == NULL)
+                set->size = set->size -1;
+
+            break;
+        }
+        past = curr;
+        curr = curr->next;
+    }
+    return;
 }
 
 void set_union(set** set1, set* set2)
 {
-	if((*set1)->capacity < set2->capacity)
-		set_union(&set2, *set1);
+    if((*set1)->capacity < set2->capacity)
+        set_union(&set2, *set1);
 
-	else
-	{
-		for(size_t i = 0; i < set2->capacity; ++i){
-			for(data* curr = set2->elements[i]->next; curr != NULL; curr = curr->next)
-				insert(*set1, curr->key);
+    else
+    {
+        for(size_t i = 0; i < set2->capacity; ++i)
+        {
+            data* curr = set2->elements[i]->next;
+            while(curr != NULL){
+                insert(*set1, curr->key);
+                curr = curr->next;
+            }
         }
-        free(set2);
+    free(set2);
     }
 }
 
+// Not so pretty print of a set
 void print_set(set* set)
 {
     printf("---------set---------\n");
@@ -188,26 +193,7 @@ void print_set(set* set)
         printf("\n");
     }
 
-    printf("-------end---------\n");
+    printf("---------end---------\n");
+    printf("\n");
 }
 
-
-int main()
-{
-    set* set = new_set(4);
-    print_set(set);
-    set = insert(set, "paris");
-    set = insert(set, "madrid");
-    set = insert(set,"aaa");
-    set = insert(set,"aaa");
-    print_set(set);
-    set = insert(set, "g");
-    print_set(set);
-    set = insert(set, "h");
-    delete(set, "paris");
-    print_set(set);
-    //printf("size = %li, capacity = %li\n",set->size,set->capacity);
-    //printf("found(paris) = %li\n", search(set, "paris", hash("paris")%set->capacity));
-    free_set(set);
-    return 0;
-}
