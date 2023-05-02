f61d8a4cb4e11d0224e6075516d45866b293714a
atti <attilio.levieils@epita.fr>
2023-05-02 17:12:33 +0200

    test for sets

---
+++ fix_set/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "set.h"
+
+int main()
+{
+    set* set = new_set(5);
+    print_set(set);
+
+    char* a = "a er";
+    char* b = "b dfdfb";
+    char* c = "c zfgc";
+    char* d = "d jozvd";
+    char* e = "e frezg";
+    char* f = "f rge";
+    char* g = "g aevd";
+    char* h = "h mop";
+    char* i = "i ivezrv";
+    char* j = "j ezvds";
+    char* k = "k zev";
+    char* l = "l vezfvds";
+    char* m = "m aQCX<";
+    char* n = "n zfdsw";
+    char* o = "o zae";
+    char* p = "p WWaQ";
+    char* q = "q mpzvsd";
+    char* r = "r juzsqd";
+    char* s = "s cvbn,";
+    char* t = "t azsdcg";
+    char* u = "u pmlkjikol";
+    char* v = "v zedszed";
+    char* w = "w pmp";
+    char* x = "x nhjk,";
+    char* y = "y ascc";
+    char* z = "z qsdsqsd";
+
+    insert_set(&set, a);
+    insert_set(&set,b);
+    insert_set(&set,c);
+    //print_set(s);
+    insert_set(&set,d);
+    insert_set(&set,e);
+    insert_set(&set,f);
+    insert_set(&set,g);
+    insert_set(&set,h);
+    insert_set(&set,i);
+    insert_set(&set,j);
+    insert_set(&set,k);
+    insert_set(&set,l);
+    insert_set(&set,m);
+    insert_set(&set,n);
+    insert_set(&set,o);
+    insert_set(&set,p);
+    insert_set(&set,q);
+    insert_set(&set,r);
+    insert_set(&set,s);
+    insert_set(&set,t);
+    insert_set(&set,u);
+    insert_set(&set,v);
+    insert_set(&set,w);
+    insert_set(&set,x);
+    insert_set(&set,y);
+    insert_set(&set,z);
+    //print_set(s);
+    /*printf("hkey(%s) = %i\n",c,hash(c));//%(s->capacity));
+    insert_set(&s,e);
+    print_set(s);
+    printf("hkey(%s) = %i\n",c,hash(c));//%(s->capacity));*/
+    return 0;
+}
---
+++ fix_set/set.c
@@ -0,0 +1,243 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+#include <string.h>
+#include "set.h"
+
+// Return the hash value of the key string
+uint32_t hash(char *key)
+{
+  //size_t i = 0;
+  uint32_t hash = 0;
+  while (*key != 0)
+  {
+    hash += *(key++);
+    hash += hash << 10;
+    hash ^= hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  hash += hash << 15;
+  return hash;
+}
+
+// Return a new set of 'capacity' capacity
+set* new_set(size_t capacity)
+{
+    set* set;
+    set = malloc(sizeof(struct set));
+    if(set == NULL)
+        errx(1,"new_set() : Error !");
+
+    set->capacity = capacity;
+    set->size = 0;
+    set->len = 0;
+
+    set->elements = malloc(sizeof(struct data*) * capacity);
+    if(set->elements == NULL)
+        errx(1,"new_set() : Error !");
+
+    //allocate each cells of the array
+    for(size_t i = 0; i < capacity; ++i)
+    {
+        data* sentinel = malloc(sizeof(struct data));
+        if(sentinel == NULL)
+            errx(1,"new_set() : Error !");
+
+        sentinel->next = NULL;
+        set->elements[i] = sentinel;
+    }
+
+    return set;
+}
+
+// Free a set
+void free_set(set* set)
+{
+    for(size_t i = 0; i < set->capacity; ++i)
+    {
+        data* curr = set->elements[i];
+        while(curr != NULL)
+        {
+            data* next = curr->next;
+            free(curr);
+            curr = next;
+        }
+    }
+    free(set->elements);
+    free(set);
+}
+
+
+// Return 1 if key with hash i is in the set, 0 otherwise
+size_t search_set(set* set, char* key)
+{
+    /*uint32_t i = hash(key) % (set->capacity);
+    //printf("%u\n", i);
+    for(data* curr = (set->elements[i])->next; curr != NULL; curr = curr->next)
+    {
+        if(strcmp(key, curr->key) == 0)
+            return 1;
+    }
+    return 0;*/
+    for(size_t i = 0; i < set->capacity; ++i)
+    {
+        data* cur = set->elements[i]->next;
+        while(cur != NULL)
+        {
+            if(strcmp(key, cur->key) == 0)
+                return 1;
+            cur = cur->next;
+        }
+    }
+    return 0;
+}
+
+
+// Return the set with key inserted in set
+// if key already in, nothing is done to the set
+void insert_set(set** set, char* key)
+{
+    uint32_t i = hash(key) % ((*set)->capacity);
+
+    if(search_set(*set, key))
+        return;
+
+    if(100 * (*set)->size / (*set)->capacity >= 75)
+        expand_set(set);
+
+    data* new_data = malloc(sizeof(struct data));
+    new_data->key = key;
+    new_data->hkey = i;
+    new_data->next = (*set)->elements[i]->next;
+
+    (*set)->elements[i]->next = new_data;
+
+    if(new_data->next == NULL)
+       (*set)->size += 1;
+    (*set)->len++;
+    print_set(*set);
+}
+
+// Double the size of a set
+void expand_set(set** p_set)
+{
+    print_set(*p_set);
+    // new set with double the capacity
+    set* n_set = new_set(((*p_set)->capacity)*2);
+
+    for(size_t i = 0; i < (*p_set)->capacity; ++i)
+    {
+        // reinsert the keys of the old set in the new set (new hash value)
+        data* curr = (*p_set)->elements[i]->next;
+        while(curr != NULL){
+            printf("\nexpand : i = %zu; \ncurrently at %s; hash = %i; prev_h = %li; new_h = %li\n",i,curr->key,hash(curr->key),hash(curr->key)%((*p_set)->capacity),hash(curr->key)%(n_set->capacity));
+            insert_set(&n_set, curr->key);
+            curr = curr->next;
+        }
+    }
+
+    free_set(*p_set);
+    *p_set = n_set;
+}
+
+// Delete the key in the set, do nothing if key not in the set
+void delete_set(set* set, char* key)
+{
+    uint32_t i = hash(key) % (set->capacity);
+
+    data* curr = set->elements[i]->next;
+    data* past = set->elements[i];
+    while(curr != NULL)
+    {
+        // key found
+        if(strcmp(curr->key,key) == 0){
+            past->next = curr->next;
+            free(curr);
+
+            set->len--;
+
+            if(set->elements[i]->next == NULL)
+                set->size = set->size -1;
+
+            break;
+        }
+        past = curr;
+        curr = curr->next;
+    }
+    return;
+}
+
+// Return and delete random key from the set
+char* pop_set(set* set)
+{
+    if(set->size == 0)
+        errx(1, "pop() : cannot pop an element from an empty set !");
+
+    char* key;
+    for(size_t i = 0; i < set->capacity; ++i)
+    {
+        data* curr = set->elements[i]->next;
+        if(curr != NULL) //first data found in the set
+        {
+            key = curr->key;
+            delete_set(set, key);
+            break;
+        }
+    }
+    return key;
+}
+
+// Return, in set1 the union of the two sets
+void union_set(set** set1, set* set2)
+{
+    for(size_t i = 0; i < set2->capacity; ++i)
+    {
+        data* curr = set2->elements[i]->next;
+        while(curr != NULL){
+            insert_set(set1, curr->key);
+            curr = curr->next;
+        }
+    }
+}
+
+// Not so pretty print of a set
+void print_set(set* set)
+{
+    printf("---------set---------\n");
+    printf("capacity = %zu ; size = %zu\n\n", set->capacity, set->size);
+    for(size_t i = 0; i < set->capacity; ++i)
+    {
+        printf("Bucket %zu: ", i + 1);
+        data* curr = set->elements[i];
+        do
+        {
+            curr = curr->next;
+            if(curr == NULL)
+                printf("[empty] ");
+            else
+                printf("[%s] ", curr->key);
+        }while(curr != NULL);
+        printf("\n");
+    }
+
+    printf("---------end---------\n");
+}
+
+//Returns the max value of a set
+char* max_set(set* set)
+{
+    char* max = "";
+    for(size_t i = 0; i < set->capacity; ++i)
+    {
+        data* curr = set->elements[i]->next;
+        while(curr != NULL)
+        {
+            if(strcmp(curr->key, max) > 0)
+                max = curr->key;
+            curr = curr->next;
+        }
+    }
+    return max;
+}
