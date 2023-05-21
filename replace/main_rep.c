5ec8c43c6800b53ec1dbcfb1ec6978977d2f8a3c
atti <attilio.levieils@epita.fr>
2023-05-21 15:31:18 +0200

    replace working

--- replace/main_rep.c
+++ replace/main_rep.c
@@ -5,78 +5,103 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gmodule.h>
+//#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-    if(argc != 3)
+    if(argc > 3)
         errx(EXIT_FAILURE,"failure");
 
-    FILE *file;
-    char *line = NULL;
-    size_t line_size = 0;
-    ssize_t len_line;
-    size_t len_pattern = 0;
-    char *pattern = argv[1];
-    char *new_word = argv[2];
-    size_t len_word = strlen(new_word);
-    int position;
-    int test = 0;
+    char* pattern = "fin";
+    char* new_word = "grand";
 
-    // Ouvrir le fichier en lecture/écriture
-    file = fopen("test.txt", "r+");
-
-    if (file == NULL)
-        errx(EXIT_FAILURE,"Error opening the file");
-
-    // Lire le fichier ligne par ligne
-    while ((len_line = getline(&line, &line_size, file)) != -1) 
-    {
-        printf("new line = %s\n",&line[0]);
-        // Recherche du mot à remplacer dans la ligne
-        test = get_pattern(line, len_line, pattern, &len_pattern);
-        while (test != -1) 
-        {
-            position += test;
-            // Calculer l'index de début et de fin du mot à remplacer
-            int start = position;
-            int end = start + len_word;
-            position = end;
-
-            printf("before replace: start = %i; end = %i; len_pattern = %li; line start = %s\n",start,end,len_pattern,&line[start]);
-            // Remplacer le mot
-            insert_string(&line[start], new_word, len_pattern, len_word);
-
-            printf("after replace: line start = %s\n",&line[start]);
-            // Chercher la prochaine occurrence du mot
-            test = get_pattern(line+end, len_line - position, pattern, &len_pattern);
-        }
-
-        // Réécrire la ligne modifiée dans le fichier
-        fseek(file, -len_line, SEEK_CUR);
-        fputs(line, file);
-        printf("end line---------------------------------\n");
-    }
-
-    // Libérer la mémoire allouée
-    free(line);
-
-    // Fermer le fichier
-    fclose(file);
+    replace("test.txt",pattern, new_word);
 
     return 0;
 }
 
-void insert_string(char* dest, char* new_word, size_t len_old_word, size_t len_new_word) 
+void replace(char* file_path, char* pattern, char* new_word)
+{
+    FILE* file;
+    FILE* tmp_file;
+
+    char* line = NULL;
+
+    size_t line_size = 0;
+    size_t len_pattern = 0;
+    size_t len_word = strlen(new_word);
+    ssize_t len_line;
+
+    int position = 0;
+    int test = 0;
+    int start;
+    int end;
+
+    file = fopen(file_path, "r");
+    tmp_file = fopen("tmp.txt", "w");
+    if(file == NULL || tmp_file == NULL)
+        errx(EXIT_FAILURE, "cannot open the files.");
+
+    while((len_line = getline(&line, &line_size, file)) != -1)
+    {
+        printf("new line = %s; of length = %li\n",line,strlen(line));
+
+        test = get_pattern(line, len_line, pattern, &len_pattern);
+        while(test != -1)
+        {
+            position += test;
+            start = position;
+            end = start+len_word;
+            position = end;
+
+            printf("before replace: start = %i; end = %i; len_pattern = %li; line start = %s\n",start,end,len_pattern,&line[start]);
+
+            insert_string(&line[start], new_word, len_pattern, len_word);
+
+            printf("after replace: line start = %s\n",&line[start]);
+
+            test = get_pattern(line+end, len_line-position, pattern , &len_pattern);
+        }
+        fputs(line, tmp_file);
+        position = 0;
+    }
+    free(line);
+    fclose(file);
+    fclose(tmp_file);
+
+    remove(file_path);
+    rename("tmp.txt",file_path);
+}
+/*
+char* get_name(char* path)
+{
+    FILE *file = fopen(path, "r");
+    if (file == NULL)
+        errx(EXIT_FAILURE, "Cannot open the file.");
+
+    int d = fileno(file);
+    if (d == -1)
+        errx(EXIT_FAILURE, "Cannot get the file descriptor.");
+
+    char name[256];
+    if (fcntl(d, F_GETPATH, name) == -1)
+        errx(EXIT_FAILURE, "CAnnot get the name of the file.");
+
+    fclose(file);
+    return name;
+}
+*/
+void insert_string(char* dest, char* new_word, size_t len_old_word, size_t len_new_word)
 {
     size_t len_dest = strlen(dest);
 
-    if (len_new_word >= len_old_word) 
+    if (len_new_word >= len_old_word)
     {
         memmove(dest + len_new_word, dest + len_old_word, len_dest - len_old_word + 1);
         memcpy(dest, new_word, len_new_word);
     }
 
-    else 
+    else
     {
         size_t diff = len_old_word - len_new_word;
         size_t new_len_dest = len_dest - diff;
@@ -97,10 +122,9 @@ int get_pattern(char* line, ssize_t len_line, char* pattern, size_t* len_pattern
     char word[len_line];
     while (i < len_line)
     {
-        //printf("word before loop : %s\n",word);
         j = 0;
         len_word = 0;
-        while(i < len_line && line[i] != ' ')
+        while(i < len_line && line[i] != ' ' && line[i] != '\n')
         {
             word[j] = line[i];
             ++j;
@@ -108,10 +132,7 @@ int get_pattern(char* line, ssize_t len_line, char* pattern, size_t* len_pattern
             ++len_word;
             //printf("word in loop: %s\n",word);
         }
-        /*
-        if(i == len_line)
-            return -1;
-*/
+
         word[j] = 0;
         printf("word after loop: %s\n",word);
 
--- replace/main_rep.h
+++ replace/main_rep.h
@@ -6,8 +6,13 @@
 #include <string.h>
 #include <gmodule.h>
 #include "../Interpretor/interpretor.h"
+//#include <fcntl.h>
+
+void replace(char* file_path, char* pattern, char* new_word);
 
 int get_pattern(char* line, ssize_t len_line, char* pattern, size_t* len_pattern);
+
+//char* get_name(char* path);
 
 void insert_string(char* dest, char* src, size_t len_dest, size_t len_src);
 #endif
